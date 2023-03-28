bf621627e555adea18b4ad00a01183cdd62b45e1
Arif Hossan <arif.hossan.eu@gmail.com>
2023-03-28 23:04:20 +0600

    Load countries from API , display countries , create and using Component from another file

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <nav className='navbar'>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
